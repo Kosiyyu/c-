4ae6183f0c99c49dbc3fdb63a212aadb992add6f
Karol Koś <kosiyyu@gmail.com>
2022-05-19 10:42:53 +0200

    Update main.cpp
    fixing degree and few little blunders ;))

--- approximation/main.cpp
+++ approximation/main.cpp
@@ -2,26 +2,28 @@
     Least-squares function approximation
 
     In order to achieve other values for the given points, you can
-    change the value of the variable n, corresponding to the degree
-    of the polynomial. Where n is an integer greater than zero in 
+    change the value of the variable n + 1, corresponding to the degree
+    of the polynomial. Where n + 1 is an integer greater than zero in
     the range of int.
 
     Program created by Karol Koś
 */
 
 #include <iostream>
+
 #include <cmath>
+
 #include <fstream>
 
 using namespace std;
 
-int n = 2;//degree 
-int m = 8;//number of points
+int n = 1; //degree
+int m = 8; //number of points
 
 double* x = new double[m];
 double* y = new double[m];
-double* a = new double[n];
-double* F = new double[n];
+double* a = new double[n + 1];
+double* F = new double[n + 1];
 double** g = new double* [m];
 
 double w(int index) {
@@ -30,9 +32,10 @@ double w(int index) {
 double fi(int index, double exponent) {
     return pow(x[index], exponent);
 }
+
 double new_y(double* a, double* x, int index) {
     double ret = 0.0;
-    for (int i = n - 1; i >= 0; i--) {
+    for (int i = n; i >= 0; i--) {
         ret += a[i] * pow(x[index], i);
     }
     return ret;
@@ -41,22 +44,20 @@ void calculate_g() {
     for (int i = 0; i < m; i++) {
         g[i] = new double[m];
     }
-    if (m > 1) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                g[j][i] = 0;
-                for (int k = 0; k < m; k++) {
-                    g[j][i] += fi(k, j) * fi(k, i);
-                }
+    for (int i = 0; i < n + 1; i++) {
+        for (int j = 0; j < n + 1; j++) {
+            g[j][i] = 0;
+            for (int k = 0; k < m; k++) {
+                g[j][i] += fi(k, j) * fi(k, i) * w(k);
             }
         }
-        for (int i = 0; i < n; i++) {
-            g[i][n] = F[i];
-        }
+    }
+    for (int i = 0; i < n + 1; i++) {
+        g[i][n + 1] = F[i];
     }
 }
 void calculate_F() {
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n + 1; i++) {
         F[i] = 0;
         for (int j = 0; j < m; j++) {
             F[i] += fi(j, i) * y[j] * w(j);
@@ -64,51 +65,51 @@ void calculate_F() {
     }
 }
 void gauss(double** arr, double* X, int n) {
-    if (m > 1) {
-        for (int i = 0; i < n; i++)
-        {
-            int maxi = i;
-            int y = arr[i][i];
-            for (int j = i + 1; j < n; j++)
-            {
-                if (abs(arr[j][i]) > y)
-                {
-                    y = arr[j][i];
-                    maxi = j;
-                }
-            }
-            for (int j = 0; j < n + 1; j++)
-            {
-                swap(arr[maxi][j], arr[i][j]);
-            }
-            for (int j = i + 1; j < n; j++)
-            {
-                double m = arr[j][i] / arr[i][i];
-                for (int k = i + 1; k < n + 1; k++)
-                {
-                    arr[j][k] = arr[j][k] - arr[i][k] * m;
-                }
-                arr[j][i] = 0;
+    for (int i = 0; i < n + 1; i++) {
+        int maxi = i;
+        int y = arr[i][i];
+        for (int j = i + 1; j < n + 1; j++) {
+            if (abs(arr[j][i]) > y) {
+                y = arr[j][i];
+                maxi = j;
             }
         }
-
-        for (int i = n - 1; i >= 0; i--)
-        {
-            X[i] = arr[i][n];
-            for (int j = i + 1; j < n; j++)
-            {
-                X[i] = X[i] - arr[i][j] * X[j];
+        for (int j = 0; j < n + 1 + 1; j++) {
+            swap(arr[maxi][j], arr[i][j]);
+        }
+        for (int j = i + 1; j < n + 1; j++) {
+            double m = arr[j][i] / arr[i][i];
+            for (int k = i + 1; k < n + 1 + 1; k++) {
+                arr[j][k] = arr[j][k] - arr[i][k] * m;
             }
-            X[i] = X[i] / arr[i][i];
+            arr[j][i] = 0;
         }
     }
+
+    for (int i = n; i >= 0; i--) {
+        X[i] = arr[i][n + 1];
+        for (int j = i + 1; j < n + 1; j++) {
+            X[i] = X[i] - arr[i][j] * X[j];
+        }
+        X[i] = X[i] / arr[i][i];
+    }
+
 }
-static void getData()
-{
+
+void printArr(double** tab, int n) {
+    for (int i = 0; i < n + 1; i++) {
+        for (int j = 0; j < n + 1 + 1; j++) {
+            cout << tab[i][j] << '\t';
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+static void getData() {
     ifstream indata;
     indata.open("data.txt");
-    if (!indata)
-    {
+    if (!indata) {
         exit(1);
     }
     for (int i = 0; i < m; i++) {
@@ -116,11 +117,11 @@ static void getData()
     }
     indata.close();
 }
-int main()
-{
+int main() {
     getData();
     calculate_F();
     calculate_g();
+    //printArr(g, n);
     gauss(g, a, n);
     cout << "Degree: " << n << endl << endl;
     cout << "Number of points: " << m << endl << endl;
@@ -130,7 +131,7 @@ int main()
     }
     cout << endl;
     cout << "Coefficients:" << endl;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n + 1; i++) {
         cout << "a[" << i << "]: " << a[i] << endl;
     }
     cout << endl;
@@ -140,12 +141,12 @@ int main()
     }
     cout << endl;
 
-    delete[]x;
-    delete[]y;
-    delete[]a;
-    delete[]F;
+    delete[] x;
+    delete[] y;
+    delete[] a;
+    delete[] F;
     for (int i = 0; i < m; i++) {
         delete[] g[i];
     }
-    delete[]g;
+    delete[] g;
 }
